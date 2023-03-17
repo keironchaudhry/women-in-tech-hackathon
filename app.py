c8f8e789e6284c8a0d59baed1fbe30b241e673d5
lmjh <lmjhemming@gmail.com>
2023-03-17 20:17:34 +0000

    Update home route to process message history

--- app.py
+++ app.py
@@ -15,22 +15,35 @@ openai.api_key = os.environ.get("OPENAI_API_KEY")
 @app.route("/", methods=["GET", "POST"])
 def home():
     if request.method == "POST":
-        # retrieve user's message from submitted form
+        # retrieve user's message and the message history from submitted form
         user_message = request.form.get("user_message")
+        message_history = request.form.get("message_history").split("|")
         # assign the AI's role - currently a fixed role, but will be changeable
         role = "NASA mathematician Katherine Johnson"
+
+        # build an array of messages to send to the API
+        # set up the AI role with the first message
+        messages = [
+            {
+                "role": "system",
+                "content": f"You are {role}. You are not an ai model. You are a helpful and enthusiastic mentor. You reply with fewer than 100 words",
+            }
+        ]
+
+        # add the message history
+        for message in message_history:
+            if message:
+                split_message = message.split(": ")
+                messages.append({"role": split_message[0], "content": split_message[1]})
+
+        # attach latest user message as last message
+        messages.append({"role": "user", "content": f"{user_message}"})
 
         try:
             # create a call to the API
             response = openai.ChatCompletion.create(
                 model="gpt-3.5-turbo",
-                messages=[
-                    {
-                        "role": "system",
-                        "content": f"You are {role}. You are not an ai model. You are a helpful and enthusiastic mentor. You reply with fewer than 100 words",
-                    },
-                    {"role": "user", "content": f"{user_message}"},
-                ],
+                messages=messages,
                 max_tokens=200,
                 temperature=0.2,
             )
@@ -38,13 +51,20 @@ def home():
             # extract the generated reply from the response from the API
             reply = response["choices"][0]["message"]["content"]
 
+            # append the user's message and the API reply to the message_history array
+            message_history.append(f"user: {user_message}")
+            message_history.append(f"assistant: {reply}")
+
         except Exception as e:
             # print any errors to the console for debugging
             print(e)
             reply = "Sorry, something went wrong. Please try again."
 
-        # render the home page with the user's message and the AI's reply
-        return render_template("home.html", user_message=user_message, reply=reply)
+        # render the home page with the message history
+        return render_template(
+            "home.html",
+            message_history=message_history,
+        )
 
     return render_template("home.html")
 
