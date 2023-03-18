cd3ae4f1ccbf43c3be9a8db7ca5fd29024b8d03e
lmjh <lmjhemming@gmail.com>
2023-03-18 16:35:14 +0000

    Add route to handle async API calls

--- app.py
+++ app.py
@@ -34,7 +34,9 @@ def home():
         for message in message_history:
             if message:
                 split_message = message.split(": ")
-                messages.append({"role": split_message[0], "content": split_message[1]})
+                messages.append(
+                    {"role": split_message[0], "content": split_message[1]}
+                )
 
         # attach latest user message as last message
         messages.append({"role": "user", "content": f"{user_message}"})
@@ -91,7 +93,9 @@ def chat():
         for message in message_history:
             if message:
                 split_message = message.split(": ")
-                messages.append({"role": split_message[0], "content": split_message[1]})
+                messages.append(
+                    {"role": split_message[0], "content": split_message[1]}
+                )
 
         # attach latest user message as last message
         messages.append({"role": "user", "content": f"{user_message}"})
@@ -127,13 +131,57 @@ def chat():
 
 @app.route("/fetch", methods=["GET", "POST"])
 def fetch():
-    if request.method == "GET":
-        message = {"greeting": "Hello from Flask!"}
-        return jsonify(message)
-
     if request.method == "POST":
-        print(request.get_json())
-        return request.get_json(), 200
+        # retrieve user's message and the message history from request data
+        message_data = request.get_json()
+        user_message = message_data["userMessage"]
+        message_history = message_data["messageHistory"].split("|")
+
+        # assign the AI's role - currently a fixed role, but will be changeable
+        role = "NASA mathematician Katherine Johnson"
+
+        # build an array of messages to send to the API
+        # set up the AI role with the first message
+        messages = [
+            {
+                "role": "system",
+                "content": f"You are {role}. You are not an ai model. You are a helpful and enthusiastic mentor. You reply with fewer than 100 words",
+            }
+        ]
+
+        # add the message history
+        for message in message_history:
+            if message:
+                split_message = message.split(": ")
+                messages.append(
+                    {"role": split_message[0], "content": split_message[1]}
+                )
+
+        # attach latest user message as last message
+        messages.append({"role": "user", "content": f"{user_message}"})
+
+        try:
+            # create a call to the API
+            response = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=messages,
+                max_tokens=200,
+                temperature=0.2,
+            )
+
+            # extract the generated reply from the response from the API
+            reply = {"message": response["choices"][0]["message"]["content"]}
+
+            # return the reply
+            return reply, 200
+
+        except Exception as e:
+            # print any errors to the console for debugging
+            print(e)
+            reply = {
+                "message": "Sorry, something went wrong. Please try again."
+            }
+            return reply, 200
 
 
 if __name__ == "__main__":
